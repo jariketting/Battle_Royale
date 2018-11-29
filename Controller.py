803f81ea10a43d00b80f5535edd871815b87e45d
Jari <personal@jariketting.com>
2018-11-29 16:35:14 +0100

    Add basic vars

--- Controller.py
+++ Controller.py
@@ -4,4 +4,7 @@ Game controller class
 
 
 class Controller:
-    
+    # these are private variables and should not be accessed outside this class.
+    _round = 1  # stores games current round
+    _players = []  # stores players in game
+    _turn = 0  # stores player that is turning ?randomize this number to have a random player start
