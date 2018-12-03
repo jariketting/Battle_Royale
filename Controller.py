4df3946f2909acc5ccff6ae3d998c312dd3bb35c
Jari <personal@jariketting.com>
2018-12-03 11:36:32 +0100

    Add next round and get round

--- Controller.py
+++ Controller.py
@@ -8,3 +8,11 @@ class Controller:
     _round = 1  # stores games current round
     _players = []  # stores players in game
     _turn = 0  # stores player that is turning ?randomize this number to have a random player start
+
+    # turn to next round
+    def next_round(self):
+        self._round += 1
+
+    # get current round
+    def get_round(self):
+        return self._round
