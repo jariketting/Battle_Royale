54f173d75e6054f654d76e08b7631183c8bc2a89
Jari <personal@jariketting.com>
2018-12-16 22:47:53 +0100

    Add images and delete grenade

--- main/Items.py
+++ main/Items.py
@@ -11,31 +11,32 @@ Get items name
 # Don't use this class in code
 class Item:
     _name = ''
+    _image = ''
 
     # get name of item
     def get_name(self):
         return self._name
+
+    def get_image(self):
+        return self._image
 
 
 # setup armor
 class Armor(Item):
     def __init__(self):
         self._name = 'Armor'
+        self._image = 'main_screen/cards/items/armor.png'
 
 
 # setup first aid kit
 class FirstAidKit(Item):
     def __init__(self):
         self._name = 'First aid kit'
+        self._image = 'main_screen/cards/items/medkit.png'
 
 
 # setup band aid
 class BandAid(Item):
     def __init__(self):
         self._name = 'Band aid'
-
-
-# setup grenade
-class Grenade(Item):
-    def __init__(self):
-        self._name = 'Grenade'
+        self._image = 'main_screen/cards/items/band_aid.png'
